bf073cc03b55164ad1eadba28165e324416896ca
alopezCSUMB <alopezmartinez@csumb.edu>
2023-03-22 15:38:14 -0700

    first commit

---
+++ file_one.py
@@ -0,0 +1,9 @@
+# Aurelio Lopez
+# Lab-17
+# 22 March 23
+# Description: create repository and use git commands to add, commit and push to Github
+
+
+# Example code to 
+for i in range(100 + 1):
+    print(i)
